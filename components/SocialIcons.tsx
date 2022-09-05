586f9caf9168dc0664696d2e7cb86e4b0f75d87b
Milad Marzideh <milad.barzideh@snapp.cab>
2022-09-05 11:32:53 +0430

    refactor: comment youtube and buymeacoffee in social icons

--- components/SocialIcons.tsx
+++ components/SocialIcons.tsx
@@ -52,17 +52,17 @@ function resolveIcon(entry: Tuple<string>): React.ReactNode {
       icon = <GithubIcon {...props} />;
       break;
 
-    case ContactType.youtube:
-      icon = <YoutubeIcon {...props} />;
-      break;
+    // case ContactType.youtube:
+    //   icon = <YoutubeIcon {...props} />;
+    //   break;
 
     case ContactType.email:
       icon = <MailIcon {...props} />;
       break;
 
-    case ContactType.buymeacoffee:
-      icon = <BuymeacoffeeIcon {...props} />;
-      break;
+    // case ContactType.buymeacoffee:
+    //   icon = <BuymeacoffeeIcon {...props} />;
+    //   break;
     default:
       break;
   }
