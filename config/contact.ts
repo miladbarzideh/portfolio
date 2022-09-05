a8df65b5c2d3f537bf048c0cd3ef3a99155a17d6
Milad Marzideh <milad.barzideh@snapp.cab>
2022-09-05 11:17:36 +0430

    refactor: add youtube and buymeacoffee to ContactType

--- config/contact.ts
+++ config/contact.ts
@@ -2,7 +2,9 @@ export enum ContactType {
   github = 'github',
   linkedin = 'linkedin',
   twitter = 'twitter',
+  youtube = 'youtube',
   email = 'email',
+  buymeacoffee = 'buymeacoffee',
 }
 
 export interface Contact {
@@ -20,6 +22,8 @@ export const contact: Contact = {
     github: 'https://github.com/miladbarzideh',
     linkedin: 'https://www.linkedin.com/in/milad-barzideh-b2870868',
     twitter: 'https://twitter.com/mimbe15',
+    youtube: '',
     email: 'mailto:barzidehmilad@gmail.com',
+    buymeacoffee: '',
   },
 };
